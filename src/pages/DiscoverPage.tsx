555689bada6ce3cc41a39207c728f341c453fe88
stanlys96 <stanlyskwok@gmail.com>
2025-11-06 22:09:09 +0700

    wlao

--- src/pages/DiscoverPage.tsx
+++ src/pages/DiscoverPage.tsx
@@ -352,7 +352,7 @@ export function DiscoverPage({ onCreatorClick }: DiscoverPageProps) {
     if (value >= 1000000) {
       return `$${(value / 1000000)?.toFixed(2)}M`;
     } else if (value >= 1000) {
-      return `$${(value / 1000)?.toFixed(1)}K`;
+      return `$${(value / 1000)?.toFixed(2)}K`;
     }
     return formatUSD(value);
   };
