ef6c3d4deed044af841c4e2472d6f61bbb56d8ae
stanlys96 <stanlyskwok@gmail.com>
2025-11-19 22:33:14 +0700

    wlao

--- src/components/icons/XIcon.tsx
+++ src/components/icons/XIcon.tsx
@@ -1,10 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
 
-export const XIcon = () => {
+interface Props {
+  className?: string;
+  color?: string;
+}
+
+export const XIcon = ({ className = "w-5 h-5", color = "white" }: Props) => {
   return (
-    <div>
-      <FontAwesomeIcon className="w-5 h-5" color="white" icon={faXTwitter} />
+    <div className="w-full h-full flex justify-center items-center">
+      <FontAwesomeIcon className={className} color={color} icon={faXTwitter} />
     </div>
   );
 };
